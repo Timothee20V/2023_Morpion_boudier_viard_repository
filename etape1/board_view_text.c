9a3b7796a8533d0d70c7ac13f9e77644a09e1f75
BOUDIER Nathanael <nathanael.boudier@gmail.com>
2023-02-02 08:05:20 +0100

    com

--- etape1/board_view_text.c
+++ etape1/board_view_text.c
@@ -29,12 +29,15 @@ void BoardView_displayEndOfGame (GameResult result)
 
 void BoardView_displayPlayersTurn (PieceType thisPlayer)
 {
+  //verifie que la valeur est bien un joueur
   assert(thisPlayer != NONE);
+  //print le joueur qui doit jouer
   printf("Player %d has to play", thisPlayer);
 }
 
 void BoardView_sayCannotPutPiece (void)
 {
+  //print le message d'erreur quand la case n'est pas vide
   printf("Cannot put piece, this square is not empty");
 }
 
